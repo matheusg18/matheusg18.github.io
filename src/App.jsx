435d7f8cca937b3e9042206d9a41c53af4b4639b
matheusg18 <matheusgabriel.18@gmail.com>
2022-02-28 14:51:46 -0300

    [Sobre] adiciona estrutura principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Hero from './components/Hero';
+import Sobre from './components/Sobre';
 
 function App() {
   return (
     <div>
       <Hero />
+      <Sobre />
     </div>
   );
 }
--- src/components/Sobre/index.jsx
+++ src/components/Sobre/index.jsx
@@ -1,7 +1,34 @@
 import React from 'react';
 
 function Sobre() {
-  return <div>Sobre</div>;
+  return (
+    <section>
+      <h2>Sobre mim</h2>
+      <img src="images/ilustração.png" alt="ilustração" />
+      <p>
+        Meu nome é Matheus Santos, tenho 21 anos e moro em Recife - PE. Sou apaixonado por
+        programação e por aprender coisas novas. Comecei minha jornada na programação em 2018 quando
+        eu aprendi C. Daí em diante estudei C++, C# (Unity), Java, Python e não parava, sempre
+        gostei muito de programar.
+      </p>
+      <p>
+        Entrei para a Trybe (escola de programação) para me tornar um desenvolvedor FullStack. Meu
+        objetivo atual é começar a minha carreira de desenvolvedor de software e enfrentar o desafio
+        que me espera.
+      </p>
+      <p>
+        No tempo livre gosto de jogar algum game, assistir um anime ou programar. E programar sempre
+        com um lofizinho né, não pode faltar.
+      </p>
+      <a
+        href="https://drive.google.com/file/d/1jh03xwlASqpnNnw1fBmsOVJwVxgFhYoj/view?usp=sharing"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Download CV
+      </a>
+    </section>
+  );
 }
 
 export default Sobre;
