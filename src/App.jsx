3b0b038542361ddb273c459021f83657c5753301
matheusg18 <matheusgabriel.18@gmail.com>
2022-02-28 16:56:36 -0300

    [Skills] adiciona estrutura principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Hero from './components/Hero';
 import Sobre from './components/Sobre';
+import Skills from './components/Skills';
 
 function App() {
   return (
     <div>
       <Hero />
       <Sobre />
+      <Skills />
     </div>
   );
 }
--- src/components/Skills/index.jsx
+++ src/components/Skills/index.jsx
@@ -1,7 +1,34 @@
 import React from 'react';
+import SkillIcon from '../SkillIcon';
 
 function Skills() {
-  return <div>Skills</div>;
+  const skillsList = [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'Jest',
+    'React',
+    'React Testing Library',
+    'Redux',
+    'SASS',
+    'Styled Components',
+    'NodeJS',
+    'MySQL',
+    'Docker',
+    'Git',
+    'Linux',
+  ];
+
+  return (
+    <section>
+      <h2>Skills</h2>
+      <div>
+        {skillsList.map((skill) => (
+          <SkillIcon key={skill} skillName={skill} />
+        ))}
+      </div>
+    </section>
+  );
 }
 
 export default Skills;
