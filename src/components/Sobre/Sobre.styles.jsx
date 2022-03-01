18ad56212605378a99ea2067284c5d9f2600e563
matheusg18 <matheusgabriel.18@gmail.com>
2022-02-28 21:31:13 -0300

    [Sobre] adiciona estilos para o mobile

---
+++ src/components/Sobre/Sobre.styles.jsx
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+
+export const SobreWrapper = styled.section`
+  width: 90%;
+  margin: 0 auto;
+  background-color: ${({ theme }) => theme.color.card};
+  border-radius: 10px;
+  padding: ${({ theme }) => theme.space(2.5)};
+  box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.25);
+`;
+
+export const SobreTextImgContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const SobreImg = styled.img`
+  max-width: 240px;
+  height: auto;
+  margin: ${({ theme }) => theme.space(4)} 0;
+`;
+
+export const SobreParagraph = styled.p`
+  margin-bottom: ${({ theme }) => theme.space(2)};
+  text-align: center;
+`;
+
+export const SobreLink = styled.a`
+  display: block;
+  width: fit-content;
+  margin: ${({ theme }) => theme.space(1)} auto 0 auto;
+  border-radius: 30px;
+  padding: ${({ theme }) => theme.space(2)} ${({ theme }) => theme.space(4)};
+  background-color: ${({ theme }) => theme.color.secondary};
+  text-decoration: none;
+  font-size: 20px;
+  font-weight: 700;
+  color: ${({ theme }) => theme.color.font};
+`;
--- src/components/Sobre/index.jsx
+++ src/components/Sobre/index.jsx
@@ -1,33 +1,44 @@
 import React from 'react';
+import {
+  SobreImg,
+  SobreLink,
+  SobreParagraph,
+  SobreTextImgContainer,
+  SobreWrapper,
+} from './Sobre.styles';
 
 function Sobre() {
   return (
-    <section>
+    <SobreWrapper>
       <h2>Sobre mim</h2>
-      <img src="images/ilustração.png" alt="ilustração" />
-      <p>
-        Meu nome é Matheus Santos, tenho 21 anos e moro em Recife - PE. Sou apaixonado por
-        programação e por aprender coisas novas. Comecei minha jornada na programação em 2018 quando
-        eu aprendi C. Daí em diante estudei C++, C# (Unity), Java, Python e não parava, sempre
-        gostei muito de programar.
-      </p>
-      <p>
-        Entrei para a Trybe (escola de programação) para me tornar um desenvolvedor FullStack. Meu
-        objetivo atual é começar a minha carreira de desenvolvedor de software e enfrentar o desafio
-        que me espera.
-      </p>
-      <p>
-        No tempo livre gosto de jogar algum game, assistir um anime ou programar. E programar sempre
-        com um lofizinho né, não pode faltar.
-      </p>
-      <a
+      <SobreTextImgContainer>
+        <SobreImg src="images/ilustração.png" alt="ilustração" />
+        <div>
+          <SobreParagraph>
+            Meu nome é Matheus Santos, tenho 21 anos e moro em Recife - PE. Sou apaixonado por
+            programação e por aprender coisas novas. Comecei minha jornada na programação em 2018
+            quando eu aprendi C. Daí em diante estudei C++, C# (Unity), Java, Python e não parava,
+            sempre gostei muito de programar.
+          </SobreParagraph>
+          <SobreParagraph>
+            Entrei para a Trybe (escola de programação) para me tornar um desenvolvedor FullStack.
+            Meu objetivo atual é começar a minha carreira de desenvolvedor de software e enfrentar o
+            desafio que me espera.
+          </SobreParagraph>
+          <SobreParagraph>
+            No tempo livre gosto de jogar algum game, assistir um anime ou programar. E programar
+            sempre com um lofizinho né, não pode faltar.
+          </SobreParagraph>
+        </div>
+      </SobreTextImgContainer>
+      <SobreLink
         href="https://drive.google.com/file/d/1jh03xwlASqpnNnw1fBmsOVJwVxgFhYoj/view?usp=sharing"
         target="_blank"
         rel="noreferrer"
       >
         Download CV
-      </a>
-    </section>
+      </SobreLink>
+    </SobreWrapper>
   );
 }
 
