458b5d72895ead5d4e81010e2d92aa16931d8dd4
matheusg18 <matheusgabriel.18@gmail.com>
2022-03-01 18:42:39 -0300

    [Header] ajusta os testes

--- src/components/Header/Header.test.jsx
+++ src/components/Header/Header.test.jsx
@@ -1,33 +1,42 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { act, render, screen } from '@testing-library/react';
 import { ThemeProvider } from 'styled-components';
 import Header from '.';
 import theme from '../../theme.styles';
 
-describe('Testes no Header', () => {
-  beforeEach(() => render(
+const renderHeader = () => {
+  render(
     <ThemeProvider theme={theme}>
       <Header />
     </ThemeProvider>,
-  ));
+  );
+};
 
+describe('Testes no Header', () => {
   it('Deve haver um link com meu nome', () => {
+    act(renderHeader);
     const nameLink = screen.getByRole('link', { name: 'Matheus' });
 
     expect(nameLink).toBeInTheDocument();
   });
 
   it('Em telas grandes deve mostrar os links de navegação', () => {
-    window.innerWidth = 1440;
-    window.dispatchEvent(new Event('resize'));
+    act(renderHeader);
+    act(() => {
+      window.innerWidth = 1440;
+      window.dispatchEvent(new Event('resize'));
+    });
     const links = screen.getAllByRole('link');
 
     expect(links).toHaveLength(4);
   });
 
   it('Em telas pequenas deve mostrar o mobile menu', () => {
-    window.innerWidth = 375;
-    window.dispatchEvent(new Event('resize'));
+    act(renderHeader);
+    act(() => {
+      window.innerWidth = 375;
+      window.dispatchEvent(new Event('resize'));
+    });
     const mobileMenu = screen.getByTestId('mobile-menu');
 
     expect(mobileMenu).toBeInTheDocument();
