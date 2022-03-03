ee6921c3a287897d60084f9eecce17b5ca9b9aa7
matheusg18 <matheusgabriel.18@gmail.com>
2022-03-03 17:33:31 -0300

    Revert "Updates"
    This reverts commit 2ed843c84ae7c8771daf62d9359821e7f8a709e0.

---
+++ src/theme.styles.js
@@ -0,0 +1,14 @@
+const theme = {
+  space: (num) => `${num * 8}px`,
+  color: {
+    primary: '#0F0E17',
+    secondary: '#FF8906',
+    font: '#FFFFFF',
+    card: '#131122',
+    skillCard: '#221A5B',
+    hover: '#da7a0b',
+    active: '#b86b14',
+  },
+};
+
+export default theme;
